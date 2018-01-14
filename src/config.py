000f51ffd303f82db3c0cd589acafa47afaeca47
Sebastian Sardina <ssardina@gmail.com>
2018-01-14 18:48:28 +1100

    Fixed configuration file example

--- src/config.py
+++ src/config.py
@@ -16,10 +16,11 @@
 # along with this program; if not, see <http://www.gnu.org/licenses/>.
 
 
-AGENT_FILE = "agent_random"     #agent filename - must be in src/agents/
-MAP_FILE  = "./gr/64room_002.map" #map filename - must be in maps (sister dir to src)
-START     = (81,101)            #coordinates of start location in (col,row) format
-GOAL      = (73,61)             #coordinates of goal location in (col,row) format
+AGENT_FILE = "agent_astar"       #agent filename - must be in src/agents/
+#AGENT_FILE = "agent_random"     #agent filename - must be in src/agents/
+MAP_FILE  = "../maps/bloodvenomfalls.map" 	#map filename - must be in maps (sister dir to src)
+START     = (128,405)            #coordinates of start location in (col,row) format
+GOAL      = (409,93)             #coordinates of goal location in (col,row) format
 
 GUI = True                      #True = show GUI, False = run on command line
 SPEED = 0.0                     #delay between displayed moves in seconds
@@ -30,6 +31,6 @@ FREE_TIME = 0.000               #Step times > FREE_TIME are timed iff REALTIME =
 DYNAMIC = False                 #Implements runtime changes found in script.py when True
 STRICT = True                   #Allows traversal of impassable cells when False (default = True)
 PREPROCESS = False              #Gives agent opportunity to preprocess map (default = False)
-#COST_MODEL = 'mixed_real'      #May be 'mixed', 'mixed_real', 'mixed_opt1' or 'mixed_opt2' (default='mixed')
-COST_FILE = 'warcraft.cost'
+#COST_MODEL = 'mixed_real'      #May be 'mixed' (default), 'mixed_real', 'mixed_opt1' or 'mixed_opt2'
+COST_FILE = "../costs/G1-W5-S10.cost"
 
